f5c79e8d0ec2a9f4e983ab1b78a30224b33139b3
Dima <yudind@gmail.com>
2021-01-21 19:23:13 +0300

    download: use $ADDR variable in URL list to specify server address

--- download_ext.sh
+++ download_ext.sh
@@ -1,1 +1,2 @@
-./core/download.sh -i download_ext.txt -d media.xiph.org --max-res 1280x768 --max-mb 1200 "$@"
+./core/download.sh -i download_ext.txt -d media.xiph.org --max-res 1280x768 --max-mb 1200 \
+    --addr https://media.xiph.org/video/derf "$@"
