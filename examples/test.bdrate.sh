85209ba35e20cd832904ce44ed771f328d127fc8
Dima <yudind@gmail.com>
2021-03-15 16:14:12 +0300

    less download content + simplify examples

--- examples/test.bdrate.sh
+++ examples/test.bdrate.sh
@@ -10,13 +10,8 @@ CODECS="$CODECS; kingsoft --preset fast"
 CODECS="$CODECS; x265 kvazaar intel_sw intel_hw h265demo h264demo"
 
 # 4 point required: QP or BR or both
-case 0 in
-    0)  PRMS="22 27 32 37" # QP        
-        VECTORS="akiyo_176x144_30fps.yuv akiyo_352x288_30fps.yuv foreman_176x144_30fps.yuv foreman_352x288_30fps.yuv"
-    ;;
-    1)  PRMS="500 1000 1500 2000" # BR
-        VECTORS="FourPeople_1280x720_30fps.yuv stockholm_ter_1280x720_30fps.yuv vidyo4_1280x720_30fps.yuv"
-    ;;
-esac
+PRMS="22 27 32 37"   # QP
+#PRMS="60 100 150 200" # BR
+VECTORS="akiyo_176x144_30fps.yuv akiyo_352x288_30fps.yuv foreman_176x144_30fps.yuv foreman_352x288_30fps.yuv"
 
 ../core/bdrate.sh -i "$VECTORS" -c "$CODECS" -p "$PRMS" -o "bdrate.log" "$@"
--- examples/test.speed.sh
+++ examples/test.speed.sh
@@ -2,17 +2,11 @@
 set -eu -o pipefail
 
 CODECS="ashevc x265 kvazaar kingsoft intel_sw intel_hw h265demo h264demo"
-
-PRMS="22 1500"
-
-VECTORS=""
-VECTORS="$VECTORS FourPeople_1280x720_30fps.yuv"
-VECTORS="$VECTORS stockholm_ter_1280x720_30fps.yuv"
+PRMS="42 1500"
+VECTORS="akiyo_176x144_30fps.yuv akiyo_352x288_30fps.yuv foreman_176x144_30fps.yuv foreman_352x288_30fps.yuv"
 
 echo "Running one test at a time to get a correct performance estimate"
-THREADS="1 2 3"
-for num_threads in $THREADS; do
-    echo "Benchmark with --threads=$num_threads"
-    ../core/testbench.sh -i "$VECTORS" -c "$CODECS" -p "$PRMS" -o report.log --ncpu 1 --threads $num_threads "$@"
-    echo ""
-done
+
+THREADS=1
+echo "Benchmark with --threads=$THREADS"
+../core/testbench.sh -i "$VECTORS" -c "$CODECS" -p "$PRMS" -o report.log --ncpu 1 --threads $THREADS "$@"
